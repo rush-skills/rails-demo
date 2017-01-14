6c5427b0c24dcf0fd81a111c9e00f55a117d9721
Ankur Singh <ankur13019@iiitd.ac.in>
2017-01-15 02:17:34 +0530

    public folder behaviour

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get 'page1', to: "pages#home"
   get 'about', to: "pages#about"
+  get 'intro', to: "pages#intro"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "pages#home"
 end
